e4cdc7896ab3e793f8eef3e1df8ae7a263af2013
Kirill <Kirillfunke@gmail.com>
2018-06-19 17:13:21 +0300

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Test website</h1>
     <p>Hello world!!!</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
