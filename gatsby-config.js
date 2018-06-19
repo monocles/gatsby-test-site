0fe3fa1fb181d01c67e9f007f32ae6b8e1e08d3c
Kirill <Kirillfunke@gmail.com>
2018-06-19 16:28:36 +0300

    init
    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+	  {
+	  	resolve: 'gatsby-plugin-react-helmet'
+	  },{
+    	resolve: `gatsby-source-contentful`,
+    	options: {
+      	spaceId: `79so78j1e542`,
+      	accessToken: `b3b1b0234d946b980809a1bde7bfd1f7f696086c03a62a3e5de1f50979ab7683`,
+    	},
+	  },
+  ],
 }
